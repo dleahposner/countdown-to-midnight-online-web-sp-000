617ec571087afe32793aff594b7224244e1fc68e
Devorah Posner <dleahposner@gmail.com>
2021-01-20 00:13:48 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,14 @@
 #write your code here
 
-def countdown
+def countdown(int)
+number = 10 
+  while number == 1 
+  number -= 1 
+  puts "#{number} SECOND(S)!"
+end 
+ "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(int)
+  sleep 5
 end
